e85255381a4c4a0713210a623d2cde0e41c097a0
Rohit Jadhav <rohit.ab.jadhav@gmail.com>
2019-11-08 14:42:11 +0530

    navbar and banner
    navbar logo visibility on scroll
    
    banner loading time reduced

--- js/on_scroll.js
+++ js/on_scroll.js
@@ -3,12 +3,18 @@
 
 
   var header = document.getElementById("navbar");
+  var logo = document.getElementById("logo");
   var sticky = header.offsetTop;
+
+  logo.style.visibility = "hidden";
   
   function myFunction() {
     if (window.pageYOffset > sticky) {
       header.classList.add("bg-transperent");
+      logo.style.visibility = "visible";
+
     } else {
       header.classList.remove("bg-transperent");
+      logo.style.visibility = "hidden";
     }
   }

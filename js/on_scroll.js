b0517065030908fb68c500487b576d11052fb486
Rohit Jadhav <rohit.ab.jadhav@gmail.com>
2019-12-02 15:51:27 +0530

    navbar fixed

--- js/on_scroll.js
+++ js/on_scroll.js
@@ -47,3 +47,10 @@
 
 
 
+  $("nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    });
+});
